e09f126eee1759e5eb8199038ea4f2a47902416c
aarochuk <andrewarochukwu@gmail.com>
2024-09-25 10:10:38 -0400

    top design done bottom left

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,25 +1,44 @@
 import "./Dashboard.css";
 import wizkid from "../assets/wizkid.jpg";
+import arrow from "../assets/down-arrow.png";
+import { useState } from "react";
 
 export default function Dashboard() {
+  const [showLog, setShowLog] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+  const [method, setMethod] = useState("name");
+
+  function addSong(){}
+  function billboard(){}
+  function applePlaylist(){}
+
   return(
     <div className="dashboardBody">
       <header>
         <h1>tuneshift</h1>
         {/* replace with users spotify image */}
-        <button style={{'display':'none'}}>logout</button>
-        <img src={wizkid} alt="holder"/>
+        <div className="headerHolder">
+          {showLog && <button>logout</button>}
+          <img className="headerImg" onClick={() => setShowLog(!showLog)} src={wizkid} alt="holder"/>
+        </div>
       </header>
-      <div>
-        <h2>Add Song</h2>
-        <input></input>
-        <button>Song Name</button>
+      <div className="addContainer">
+        <h2>Add Song(s)</h2>
+        <input placeholder="Enter song name" className="songInput"/>
+        {/* make the values of the select proper */}
+        <select value={"blah"}>
+          <option value="song">Song Name</option>
+          <option value="billboard">Add Billboard hot 100</option>
+          <option value="apple">Add Apple Playlist</option>
+        </select>
       </div>
       <div>
         <div>
           <p>Name</p>
           <p>Album</p>
           <p>Release Date</p>
+          <p>Genre</p>
+          <p>Time</p>
         </div>
         {/* create display for if there are no songs that says there are no songs added yet or something */}
         <div className="songs">
