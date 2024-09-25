a9cf3ccb30c27a4ab9f9153ab5f23fe9e7d4dd38
aarochuk <andrewarochukwu@gmail.com>
2024-09-25 16:50:47 -0400

    just pushing cause y not

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -32,60 +32,116 @@ export default function Dashboard() {
           <option value="apple">Add Apple Playlist</option>
         </select>
       </div>
-      <div>
-        <div>
-          <p>Name</p>
-          <p>Album</p>
-          <p>Release Date</p>
-          <p>Genre</p>
-          <p>Time</p>
+      <div className="songsHolder">
+        <div className="songsHeader">
+          <div className="songNameHolder">
+            <p>Name</p>
+          </div>
+          <div className="otherSongDetails">
+            <p>Time</p>
+            <p>Album</p>
+            <p>Genre</p>
+          </div>
         </div>
         {/* create display for if there are no songs that says there are no songs added yet or something */}
         <div className="songs">
           <div className="songCard">
-            <img src={wizkid}/>
-            <div>
-              <p>Essense</p>
-              <p>Wizkid, Tems</p>
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
             </div>
-            <p>Made in Lagos</p>
-            <p>January 1st, 2021</p>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
           </div>
           <div className="songCard">
-            <img src={wizkid}/>
-            <div>
-              <p>Essense</p>
-              <p>Wizkid, Tems</p>
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
             </div>
-            <p>Made in Lagos</p>
-            <p>January 1st, 2021</p>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
           </div>
           <div className="songCard">
-            <img src={wizkid}/>
-            <div>
-              <p>Essense</p>
-              <p>Wizkid, Tems</p>
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
             </div>
-            <p>Made in Lagos</p>
-            <p>January 1st, 2021</p>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
           </div>
           <div className="songCard">
-            <img src={wizkid}/>
-            <div>
-              <p>Essense</p>
-              <p>Wizkid, Tems</p>
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
             </div>
-            <p>Made in Lagos</p>
-            <p>January 1st, 2021</p>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
           </div>
           <div className="songCard">
-            <img src={wizkid}/>
-            <div>
-              <p>Essense</p>
-              <p>Wizkid, Tems</p>
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
             </div>
-            <p>Made in Lagos</p>
-            <p>January 1st, 2021</p>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
+          </div>
+          <div className="songCard">
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
+            </div>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
+          </div>
+          <div className="songCard">
+            <div className="songNameHolder">
+              <img src={wizkid}/>
+              <div>
+                <p>Essense</p>
+                <p>Wizkid, Tems</p>
+              </div>
+            </div>
+            <div className="otherSongDetails">
+              <p>3:33</p>
+              <p>Made in Lagos</p>
+              <p>Afrobeat</p>
+            </div>
           </div>
         </div>
         <div>
